e3a3548227f5b206cc8346c9eed0ab929891d1e4
hridkamolbiswas <hrid.bis@outlook.com>
2020-06-06 21:28:50 +0200

    good

--- simulation/objects.py
+++ simulation/objects.py
@@ -3,21 +3,23 @@ import numpy as np
 from config import tick_to_time, time_to_tick
 
 class Sim:
-    def __init__(self):
+    def __init__(self, env):
+        self.env = env
         self.n_driving=[]
         self.n_parking=[]
         self.car_list=[]
 
 class Car:
     
-    def __init__(self, env, start_time, parking_time, driving_time):
-        self.env = env
+    def __init__(self, sim, start_time, parking_time, driving_time):
+        self.sim = sim
         self.start_time = start_time
         self.parking_time = np.random.randint(3,6)
         self.driving_time =  np.random.randint(10,15)
         self.is_driving = False
         self.is_parking = True
-        self.action = env.process(self.car_working())
+        self.sim.car_list
+        self.action = self.sim.env.process(self.car_working())
 
 
     def car_working(self):
@@ -25,17 +27,10 @@ class Car:
             #print('\tStart parking at %s' % tick_to_time(self.env.now))
             self.is_parking = True
             self.is_driving = False
-            yield self.env.timeout(time_to_tick(self.parking_time))
+            yield self.sim.env.timeout(time_to_tick(self.parking_time))
 
             self.is_parking = False
             self.is_driving = True
             #print('Start driving at %s' % tick_to_time(self.env.now))
-            yield self.env.timeout(time_to_tick(self.driving_time))
+            yield self.sim.env.timeout(time_to_tick(self.driving_time))
 
-def reporting(env, car, sim):
-    while True:
-
-        driving_drivers = len([driver for driver in sim.car_list if driver.is_driving])
-        driving_parking = len([driver for driver in sim.car_list if driver.is_parking])
-        print(f"\t{tick_to_time(env.now)} driving {driving_drivers} parking {driving_parking}") 
-        yield env.timeout(time_to_tick(1))
--- simulation/simu_main.py
+++ simulation/simu_main.py
@@ -3,39 +3,38 @@ from datetime import date, time,datetime,timedelta
 import numpy as np
 
 from config import config, tick_to_time, time_to_tick, drivers
-from objects import Sim, Car, reporting
+from objects import Sim, Car
 
-
-env = simpy.Environment()
-sim= Sim()
-
-
-def shiftplanning(env, drivers_log):
+def shiftplanning(sim):
 
     while True:
-        if tick_to_time(env.now) in drivers:
-            print(f"{tick_to_time(env.now)} drives starting to wokr")
-            ob = Car(env, tick_to_time(env.now),2,10)
-            drivers_log.append(ob)
-            print(drivers_log)
-            for item in drivers_log:
-                print('info:',item.start_time, item.parking_time, item.driving_time)
-                print('\t',item.car_working())
-        yield env.timeout(1)
-    
+        if tick_to_time(sim.env.now) in drivers:
+            print(f"{tick_to_time(sim.env.now)} drives starting to work")
+            ob = Car(sim, tick_to_time(sim.env.now),2,10)
+            sim.car_list.append(ob)
 
-# for i in range(0,len(drivers)):
-#     ob = Car(env,2,10)
-#     sim.car_list.append(ob)
+        yield sim.env.timeout(1)
 
+def reporting(sim):
+    while True:
 
-# car = Car(env,2,5)
-# env.process(reporting(env, ob, sim))
+        driving_drivers = len([driver for driver in sim.car_list if driver.is_driving])
+        driving_parking = len([driver for driver in sim.car_list if driver.is_parking])
+        print(f"\t{tick_to_time(env.now)} driving {driving_drivers} parking {driving_parking}") 
+        yield sim.env.timeout(1)
 
+def tourplan(sim):
+    pass
 
+env = simpy.Environment()
+sim= Sim(env)
+
+PASSENGER = 100
+SEAT_LIMIT = 25
 
 drivers_log = []
-env.process(shiftplanning(env, drivers_log))
+env.process(shiftplanning(sim))
+env.process(reporting(sim))
 
 env.run(until=time_to_tick(31))
 
