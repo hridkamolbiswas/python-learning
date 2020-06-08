393e88263046529c99969af67d04d0a2df652fce
hridkamolbiswas <hrid.bis@outlook.com>
2020-06-08 23:43:26 +0200

    fdd

--- miscellaneous/join.py
+++ miscellaneous/join.py
@@ -1,1 +1,16 @@
-print('hello')
+import os
+
+
+cdir = os.getcwd()
+os_name = os.name
+
+print(cdir,'\n', os_name)
+if os.path.exists("text.txt"):
+    print('yes')
+else:
+    print('No')
+
+s = 'iam hrid'
+print(s.split())
+print('_'.join(s.split()))
+print(':'.join(s))
