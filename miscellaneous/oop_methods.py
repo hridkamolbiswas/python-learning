8c3d42062ade6201457e461f6971491161c6272f
hridkamolbiswas <hrid.bis@outlook.com>
2020-06-09 21:56:27 +0200

    __str__repr__

---
+++ miscellaneous/oop_methods.py
@@ -0,0 +1,41 @@
+class Student:
+
+    SCHOOL = "UNIPB"
+
+    def __init__(self, name, roll):
+        self.name = name
+        self.roll = roll
+        self.email = self.get_email()
+
+    def __str__(self):
+        return f"{self.name} with {self.roll} object"
+
+
+    def __repr__(self):
+        return f"{self.name} has roll number {self.roll} with repr"
+
+    def get_email(self):
+        self.first = self.name.split()[0]
+        try:
+            self.last = self.name.split()[1]
+        except Exception as e:
+            self.last = ""
+            #raise IndexError ("please give second name")
+        return self.first+"."+self.last+"@gmail.com"+self.SCHOOL
+
+    def set_roll(self, roll):
+        self.roll = roll
+
+    @classmethod
+    def get_uni(cls):
+        #ob = Student("yasemin fonk")
+        #print(ob)
+        return cls.SCHOOL
+
+
+ob = Student("hrid Biswas", 200916047)
+print(ob)
+print(repr(ob))
+# print(ob.email)
+# #print(ob.get_email())
+# print(Student.get_uni())
